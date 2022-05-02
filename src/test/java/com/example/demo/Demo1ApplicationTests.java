3d8c424b70b15f2f7376c63f1a9d17672f981271
Gabriel Cortez <gabriel_cortez@epam.com>
2022-05-02 17:50:18 -0500

    add comment

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Service.UserService;
 
 @SpringBootTest
 class Demo1ApplicationTests {
 
+	@Autowired
+	UserService us;
+	
 	@Test
 	void contextLoads() {
+		
+		List<User> lista = new ArrayList<>();
+		
+		lista=us.getUsers("Gabriel", "Cortez");
+		
+		assertEquals(lista.get(0).getApellido(), "Cortez");
+		assertEquals(lista.get(0).getNombre(), "Gabriel");
+		
 	}
 
+	@Test
+	void fail() {
+		
+		List<User> lista = new ArrayList<>();
+		
+		lista=us.getUsers("s", "s");
+		
+		assertEquals(lista.size(), 0);
+		
+	}
 }
